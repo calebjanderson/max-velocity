c8b58b092a91d453507ed4a7a2b7232b61396989
calebjanderson <caleb.anderson130@gmail.com>
2018-01-09 13:55:57 -0600

    Add rythm game logic

--- client/spin/app.js
+++ client/spin/app.js
@@ -14,7 +14,9 @@ var slambar;
 var cursors;
 var pausePoints = [ 0.5654866776461306, 2.6203209836607546, -1.5466526054757181 ];
 var oldVelocity;
-
+var strMap = { 'left': 37, 'up': 38, 'right': 39, 'down': 40 }
+var directions = { 38:  ['down', 'left', 'right' ], 40: ['up', 'left', 'right'], 37: ['up', 'down', 'right'], 39: ['up', 'down', 'left'] }
+var target = 38
 function create() {
   spinning = false
   game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -22,6 +24,8 @@ function create() {
   spinner = game.add.sprite(300, 270, 'spinner');
   slambar = game.add.sprite(180, 550,'slambar')
   button = game.add.button(650, 50, 'startButton', toggleSpinner, this);
+  scoreText = game.add.text(545, 176, 'Current Speed: 0', { fontSize: '24px', fill: 'white' });
+  direction = game.add.text(600, 476, 'up', { fontSize: '24px', fill: 'white' });
 
   game.physics.enable(spinner, Phaser.Physics.ARCADE);
   spinner.anchor.set(0.579952267, 0.5)
@@ -33,12 +37,77 @@ function create() {
     if(!spinning) {
       spinner.body.angularVelocity = oldVelocity || 15
       spinning = true
+      scoreText.text = 'Current Speed: ' + spinner.body.angularVelocity;
     } else {
       oldVelocity = spinner.body.angularVelocity
       spinner.body.angularVelocity = 0
       spinning = false
+      scoreText.text = 'Current Speed: ' + spinner.body.angularVelocity;
       return
     }
+  })
+  console.log(cursors)
+  cursors.up.onDown.add((e) => {
+    if(!spinning) {
+      return
+    }
+    console.log('is down: ', e.keyCode, target)
+    if(e.keyCode === target) {
+      var randomIndex = Math.round(Math.random() * 1000 % 2)
+      direction.text = directions[target][randomIndex]
+      target = strMap[direction.text]
+      spinner.body.angularVelocity += 25;
+    } else {
+      spinner.body.angularVelocity -= 15;
+    }
+
+  })
+  cursors.down.onDown.add((e) => {
+    if(!spinning) {
+      return
+    }
+    console.log('is down: ', e.keyCode, target)
+    if(e.keyCode === target) {
+      var randomIndex = Math.round(Math.random() * 1000 % 2)
+      direction.text = directions[target][randomIndex]
+      target = strMap[direction.text]
+      spinner.body.angularVelocity += 25;
+    } else {
+      spinner.body.angularVelocity -= 15;
+    }
+
+  })
+  cursors.left.onDown.add((e) => {
+    if(!spinning) {
+      return
+    }
+    console.log('is down: ', e.keyCode, target)
+    if(e.keyCode === target) {
+      var randomIndex = Math.round(Math.random() * 1000 % 2)
+      direction.text = directions[target][randomIndex]
+      target = strMap[direction.text]
+      spinner.body.angularVelocity += 25;
+    } else {
+      spinner.body.angularVelocity -= 15;
+    }
+
+
+  })
+  cursors.right.onDown.add((e) => {
+    if(!spinning) {
+      return
+    }
+    console.log('is down: ', e.keyCode, target)
+    if(e.keyCode === target) {
+      var randomIndex = Math.round(Math.random() * 1000 % 2)
+      direction.text = directions[target][randomIndex]
+      target = strMap[direction.text]
+      spinner.body.angularVelocity += 25;
+    } else {
+      spinner.body.angularVelocity -= 15;
+    }
+
+
   })
   // console.log(spinner)
   // cursors.l
@@ -46,12 +115,16 @@ function create() {
 }
 
 function update() {
-
-  if (cursors.left.isDown || cursors.up.isDown) {
-    spinner.body.angularVelocity -= 1;
-  } else if (cursors.right.isDown || cursors.down.isDown) {
-    spinner.body.angularVelocity += 1;
+  if(!spinning) {
+    return
   }
+  scoreText.text = 'Current Speed: ' + spinner.body.angularVelocity;
+
+  // if (cursors.left.isDown || cursors.up.isDown) {
+  //   spinner.body.angularVelocity -= 1;
+  // } else if (cursors.right.isDown || cursors.down.isDown) {
+  //   spinner.body.angularVelocity += 1;
+  // }
   var match = pausePoints.find(n => {
     if(spinner.rotation <= n + 0.05 && spinner.rotation >= n - 0.05) {
       return n
