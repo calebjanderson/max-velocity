faf6c064e367902792fb80324d96f4f8a9a46155
calebjanderson <caleb.anderson130@gmail.com>
2018-01-11 14:07:22 -0600

    Fix favicon

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/favicon.ico'))
+})
+
 app.use(express.static(path.join(__dirname, '../client')))
 app.use('/scripts', express.static(path.join(__dirname, '../node_modules')))
 const PORT = process.env.PORT || 3030
