73a520e9e775958e95d87c56b3a816c867f29dd5
calebjanderson <caleb.anderson130@gmail.com>
2018-01-23 11:09:35 -0600

    Cleanup/comment code

--- client/spin/app.js
+++ client/spin/app.js
@@ -1,8 +1,6 @@
 setTimeout(() => {
   var game = new Phaser.Game(1300, 600, Phaser.AUTO, 'spin', { preload: preload, create: create, update: update, render: render });
 
-  console.log(moment)
-
   function preload() {
     game.load.image('spinner', 'assets/Yellow_Fidget_Spinner.png');
     game.load.image('startButton', 'assets/play-button.png');
@@ -27,8 +25,65 @@ setTimeout(() => {
   var strMap = { 'left': 37, 'up': 38, 'right': 39, 'down': 40 }
   var directions = { 38:  ['down', 'left', 'right' ], 40: ['up', 'left', 'right'], 37: ['up', 'down', 'right'], 39: ['up', 'down', 'left'] }
   var target = 38;
-  var temp
+  var temp;
   var bestTimeValue = Infinity;
+
+  function create() {
+    // Set initial time, ensure spinning is false,  set up game physics engine
+    time = moment();
+    spinning = false;
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    // Initialize fidget spinner sprite
+    spinner = game.add.sprite(250, 270, 'spinner');
+    spinner.anchor.set(0.579952267, 0.5);
+    game.physics.enable(spinner, Phaser.Physics.ARCADE);
+
+    // Uncomment for limited slambar functionality
+    // slambar = game.add.sprite(180, 550, 'slambar')
+    // direction2 = game.add.sprite(600, 550, 'directions')
+
+    // Add all initial buttons and text
+    startButton = game.add.button(182, 201, 'startButton', toggleSpinner, this);
+    speed = game.add.text(605, 163, 'Speed: 0', { font: '16px Press Start 2P', fill: 'white' });
+    direction = game.add.text(605, 336, 'Click Play to start!', { font: '16px Press Start 2P', fill: 'white' });
+    bestTime = game.add.text(605, 195, 'Fastest Time: \n  --:--:--', { font: '16px Press Start 2P', fill: 'white' })
+    rules = game.add.text(605, 396, 'Rules:\nGet the spinner to max veloctiy (1000)\nUse arrow keys to increase speed', { font: '16px Press Start 2P', fill: 'white' })
+    Timer = game.add.text(605, 256, '  Timer: \n 00:00:00', { font: '16px Press Start 2P', fill: 'white' });
+    Timer.visible = false
+
+    // Set up Phaser keys
+    cursors = game.input.keyboard.createCursorKeys();
+    spaceBar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    spaceBar.onDown.add(() => {
+      if(!spinning) {
+        // spinner.body.angularVelocity = oldVelocity || 15
+        // spinning = true
+        // speed.text = 'Speed: ' + spinner.body.angularVelocity;
+        spinning = true;
+        startButton.visible = false;
+
+        Timer.visible = true
+        startTimer();
+        direction.text = 'Press this key: \n ' + '  ' + (temp || 'up') + ' ' + (frameMap[temp] || '↑');
+        spinner.body.angularVelocity = 12;
+      } else {
+        oldVelocity = spinner.body.angularVelocity;
+        spinner.body.angularVelocity = 0;
+        spinning = false;
+        speed.text = 'Speed: ' + spinner.body.angularVelocity;
+        return;
+      }
+    })
+
+    // Add scoring to arrow keys
+    cursors.up.onDown.add(score)
+    cursors.down.onDown.add(score)
+    cursors.left.onDown.add(score)
+    cursors.right.onDown.add(score)
+
+  }
 
   function score(e) {
     if(!spinning) {
@@ -46,60 +101,11 @@ setTimeout(() => {
 
       direction.text = 'Press this key: \n ' + '  ' + temp + ' ' + frameMap[temp]
 
-      // direction2.frame = frameMap.indexOf(direction.text)
       spinner.body.angularVelocity += 25;
     } else {
       // Wrong input, decrease speed
       spinner.body.angularVelocity -= 15;
     }
-
-  }
-
-  function create() {
-    time = moment();
-    spinning = false
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    spinner = game.add.sprite(250, 270, 'spinner');
-    spinner.anchor.set(0.579952267, 0.5)
-    game.physics.enable(spinner, Phaser.Physics.ARCADE);
-    // slambar = game.add.sprite(180, 550, 'slambar')
-    // direction2 = game.add.sprite(600, 550, 'directions')
-    startButton = game.add.button(182, 201, 'startButton', toggleSpinner, this);
-    speed = game.add.text(605, 163, 'Speed: 0', { font: '16px Press Start 2P', fill: 'white' });
-    direction = game.add.text(605, 336, 'Click Play to start!', { font: '16px Press Start 2P', fill: 'white' });
-    bestTime = game.add.text(605, 195, 'Fastest Time: \n  --:--:--', { font: '16px Press Start 2P', fill: 'white' })
-    rules = game.add.text(605, 396, 'Rules:\nGet the spinner to max veloctiy (1000)\nUse arrow keys to increase speed', { font: '16px Press Start 2P', fill: 'white' })
-    Timer = game.add.text(605, 256, '  Timer: \n 00:00:00', { font: '16px Press Start 2P', fill: 'white' });
-    Timer.visible = false
-
-    cursors = game.input.keyboard.createCursorKeys();
-    tagButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-    tagButton.onDown.add(() => {
-      if(!spinning) {
-        // spinner.body.angularVelocity = oldVelocity || 15
-        // spinning = true
-        // speed.text = 'Speed: ' + spinner.body.angularVelocity;
-        spinning = true
-        startButton.visible = false
-
-        Timer.visible = true
-        startTimer()
-        direction.text = 'Press this key: \n ' + '  ' + (temp || 'up') + ' ' + (frameMap[temp] || '↑')
-        spinner.body.angularVelocity = 12
-      } else {
-        oldVelocity = spinner.body.angularVelocity
-        spinner.body.angularVelocity = 0
-        spinning = false
-        speed.text = 'Speed: ' + spinner.body.angularVelocity;
-        return
-      }
-    })
-    cursors.up.onDown.add(score)
-    cursors.down.onDown.add(score)
-    cursors.left.onDown.add(score)
-    cursors.right.onDown.add(score)
 
   }
 
@@ -109,31 +115,34 @@ setTimeout(() => {
   }
 
   function victory() {
-    spinning = false
-    startTime = null
-    startButton.visible = true
+    spinning = false;
+    startTime = null;
+    startButton.visible = true;
 
     if(timePassed < bestTimeValue) {
-      direction.text = 'You win! New record!'
-      bestTimeValue = timePassed
-      bestTime.text = 'Fastest Time: \n  ' + moment(bestTimeValue).format('mm:ss:SS')
+      direction.text = 'You win! New record!';
+      bestTimeValue = timePassed;
+      bestTime.text = 'Fastest Time: \n  ' + moment(bestTimeValue).format('mm:ss:SS');
     } else {
-      direction.text = 'You win!'
+      direction.text = 'You win!';
     }
 
-    timePassed = 0
+    timePassed = 0;
   }
 
   function update() {
-    time = moment()
+    time = moment();
+
     if(startTime) {
-      timePassed += time.diff(startTime, 'ss')
-      Timer.text = 'Timer: \n ' + moment(timePassed).format('mm:ss:SS')
-      startTime = time
-    }
+      timePassed += time.diff(startTime, 'ss');
+      Timer.text = 'Timer: \n ' + moment(timePassed).format('mm:ss:SS');
+      startTime = time;
+    };
+
     if(!spinning) {
-      return
-    }
+      return;
+    };
+
     speed.text = 'Speed: ' + spinner.body.angularVelocity;
 
     // if (cursors.left.isDown || cursors.up.isDown) {
@@ -165,16 +174,16 @@ setTimeout(() => {
 
   function toggleSpinner () {
     if(!spinning) {
-      spinner.body.angularVelocity = 8
+      spinner.body.angularVelocity = 8;
     } else {
-      spinner.body.angularVelocity = 0
+      spinner.body.angularVelocity = 0;
     }
 
-    spinning = !spinning
-    startButton.visible = false
+    spinning = !spinning;
+    startButton.visible = false;
 
-    Timer.visible = true
-    startTimer()
-    direction.text = 'Press this key: \n ' + '  ' + (temp || 'up') + ' ' + (frameMap[temp] || '↑')
+    Timer.visible = true;
+    startTimer();
+    direction.text = 'Press this key: \n ' + '  ' + (temp || 'up') + ' ' + (frameMap[temp] || '↑');
   }
-}, 200)
+}, 200);
